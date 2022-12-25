da9038526de9fd225c11a6e2a923017a1b8e2dd8
Nikolay Petrov <nikolay.a.petrov@intel.com>
2022-12-25 20:32:20 +0000

    Updating onedal dispatching

--- cpp/oneapi/dal/backend/dispatcher.hpp
+++ cpp/oneapi/dal/backend/dispatcher.hpp
@@ -289,11 +289,11 @@ inline constexpr auto dispatch_by_cpu(const context_cpu& ctx, Op&& op) {
     ONEDAL_IF_CPU_DISPATCH_AVX2(
         if (test_cpu_extension(cpu_ex, cpu_extension::avx2)) { return op(cpu_dispatch_avx2{}); })
     ONEDAL_IF_CPU_DISPATCH_AVX(
-        if (test_cpu_extension(cpu_ex, cpu_extension::avx)) { return op(cpu_dispatch_avx{}); })
+        if (test_cpu_extension(cpu_ex, cpu_extension::avx)) { return op(cpu_dispatch_sse42{}); })
     ONEDAL_IF_CPU_DISPATCH_SSE42(
         if (test_cpu_extension(cpu_ex, cpu_extension::sse42)) { return op(cpu_dispatch_sse42{}); })
     ONEDAL_IF_CPU_DISPATCH_SSSE3(
-        if (test_cpu_extension(cpu_ex, cpu_extension::ssse3)) { return op(cpu_dispatch_ssse3{}); })
+        if (test_cpu_extension(cpu_ex, cpu_extension::ssse3)) { return op(cpu_dispatch_default{}); })
     return op(cpu_dispatch_default{});
 }
 
