779ba3e5107df2bd51a74e96d1331435f16d0f05
Nikolay Petrov <nikolay.a.petrov@intel.com>
2022-12-26 19:53:45 +0000

    fixing clang-format

--- cpp/daal/include/services/internal/daal_kernel_defines.h
+++ cpp/daal/include/services/internal/daal_kernel_defines.h
@@ -107,12 +107,11 @@ case cpuType:                                                                   
 
 #if defined(DAAL_KERNEL_AVX)
     #undef DAAL_KERNEL_BUILD_MAX_INSTRUCTION_SET_ID
-    #define DAAL_KERNEL_BUILD_MAX_INSTRUCTION_SET_ID          daal::avx
-    #define DAAL_KERNEL_AVX_ONLY(something)                   , something
-    #define DAAL_KERNEL_AVX_ONLY_CODE(...)                    __VA_ARGS__
-    #define DAAL_KERNEL_AVX_CONTAINER(ContainerTemplate, ...) , DAAL_KERNEL_CONTAINER_TEMPL(ContainerTemplate, avx, __VA_ARGS__)
-    #define DAAL_KERNEL_AVX_CONTAINER1(ContainerTemplate, ...) \
-        extern template class DAAL_KERNEL_CONTAINER_TEMPL(ContainerTemplate, avx, __VA_ARGS__);
+    #define DAAL_KERNEL_BUILD_MAX_INSTRUCTION_SET_ID           daal::avx
+    #define DAAL_KERNEL_AVX_ONLY(something)                    , something
+    #define DAAL_KERNEL_AVX_ONLY_CODE(...)                     __VA_ARGS__
+    #define DAAL_KERNEL_AVX_CONTAINER(ContainerTemplate, ...)  , DAAL_KERNEL_CONTAINER_TEMPL(ContainerTemplate, avx, __VA_ARGS__)
+    #define DAAL_KERNEL_AVX_CONTAINER1(ContainerTemplate, ...) extern template class DAAL_KERNEL_CONTAINER_TEMPL(ContainerTemplate, avx, __VA_ARGS__);
     #define DAAL_KERNEL_AVX_CONTAINER_CASE(ContainerTemplate, ...)                                 \
     case avx:                                                                                      \
         _cntr = (new DAAL_KERNEL_CONTAINER_TEMPL(ContainerTemplate, sse42, __VA_ARGS__)(daalEnv)); \
